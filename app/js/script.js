8acc6826670b7410070bbb043b0e4b237fbfd10a
Paulo <cezarleigos28@outlook.com>
2021-09-17 09:20:53 -0300

    v0.02 -  Defining project
    This time we start defining a few more elements and how our Chat will behave within the code and on the embed page.

---
+++ app/js/script.js
@@ -0,0 +1,20 @@
+
+var chat_controll = false; 
+
+function toggle_chat(){
+
+   //element opening and closing control flow...
+
+}
+
+try{
+
+}catch(e){
+
+	document.getElementById("nav-chat").addEventListener(){
+
+         //closing chat with "Close" button...
+
+     }
+
+ }
---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exemple</title>
+	<link rel="stylesheet" type="text/css" href="chat-php-js.css">
+</head>
+<body>
+    
+
+	<script type="text/javascript" src="chat-php-js.php"></script>
+</body>
+</html>
