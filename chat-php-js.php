8bea0c5e2b204ff0b39535d66a253f6d349a0105
Cezar Leigos <cezarleigos28@outlook.com>
2021-09-19 17:08:15 -0300

    v0.04 - Creating dashboard
    This time we started to create the dashboard where the user can type and chat with another user by typing and sending messages on the dashboard.

---
+++ chat-php-js.php
@@ -0,0 +1,15 @@
+<div class="cpj-nav">
+	<div class="cpj-nav-painel">
+	    <div class="cpj-nav-painel-title"></div>
+	    <div class="cpj-nav-painel-context">
+	    	<div class="cpj-nav-painel-mensage1">User 1</div>
+	    	<div class="cpj-nav-painel-mensage2">User 2</div>
+	    </div>
+	    <div class="cpj-nav-painel-user">
+	    	 <div class="cpj-nav-painel-group">
+	    	 	<input type="" name="" class="cpj-nav-painel-input">
+	    	 	<button class="cpj-nav-painel-button">Ok</button>
+	    	 </div>
+	    </div>
+    </div>
+</div>
