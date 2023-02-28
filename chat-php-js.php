3787d015ea3b84c24091d37f78621e186b5128bd
Paulo Cezar <32718867+paulocezarphp@users.noreply.github.com>
2023-02-28 20:54:38 -0300

    v0.08 - Fixing part of the structure
    This time we made one more change in the structure of the project to correct some points that could become problematic in the future.

--- chat-php-js.php
+++ chat-php-js.php
@@ -18,7 +18,7 @@
 	    </div>
 	    <div class="cpj-nav-painel-user">
 	    	 <div class="cpj-nav-painel-group">
-	    	 	<input type="" name="" class="cpj-nav-painel-input">
+	    	 	<input type="" name="" class="cpj-nav-painel-input" placeholder="Write something...">
 	    	 	<button class="cpj-nav-painel-button">Ok</button>
 	    	 </div>
 	    </div>
--- index.php
+++ index.php
@@ -4,10 +4,11 @@
 	<title>Exemple</title>
 	<link rel="stylesheet" type="text/css" href="chat-php-js.css">
 </head>
-<body>
+<body style="background-color: #111;">
     
     <?php include("chat-php-js.php"); ?>
-
+    
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
 	<script type="text/javascript" src="chat-php-js.js"></script>
 </body>
 </html>
